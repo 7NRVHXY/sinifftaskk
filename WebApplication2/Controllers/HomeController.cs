28343889f131c57f010d2a532f9c079e420ad880
Sam1rrr <samiroruclu10@gmail.com>
2023-11-29 13:32:42 +0400

    task

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using LugxGaming.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dt = await SqlHelper.GetDatas("SELECT * FROM USERSS");
+            return View(dt);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
